1b3b9de3d2343485ff84e91b34e6f233aa3f19b7
moreroron <moreroron@gmail.com>
2019-11-03 18:34:46 +0200

    init commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../src/views')
+const partialsPath = path.join(__dirname, '../src/views/partials')
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup staic directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('/home', (req, res) => {
+    res.render('index', { message: 'welcome !' });
+})
+
+app.listen(3000, () => console.log('Forecaster is live on port 3000'));
